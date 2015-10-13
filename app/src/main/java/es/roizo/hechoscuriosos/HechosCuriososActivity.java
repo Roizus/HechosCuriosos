95c415b04c7780ec6fc0ae1ec1eaf40f873ca348
Álvaro <Alvaro@MacBook-Pro-de-Alvaro-3.local>
2015-10-13 19:54:34 +0200

    Initial commit

---
+++ app/src/main/java/es/roizo/hechoscuriosos/HechosCuriososActivity.java
@@ -0,0 +1,13 @@
+package es.roizo.hechoscuriosos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HechosCuriososActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hechos_curiosos);
+    }
+}
